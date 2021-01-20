80d00367abf6e6a62d4e6468207d5cabdd87aee3
Helen Lily Hu <hhusthere@gmail.com>
2021-01-19 23:43:04 -0600

    Start creating game board

--- scripts/game.js
+++ scripts/game.js
@@ -1,1 +1,9 @@
-console.log("test")
+// initialize game board
+const WIDTH = 10;
+const HEIGHT = 20;
+for (var i = 0; i < WIDTH * HEIGHT; i++) {
+  var pixel = document.createElement('div');
+  pixel.classList.add('test-div');
+  document.body.appendChild(pixel);
+}
+console.log('clear');
