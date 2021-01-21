672a02302d9da68eb89559ab82f6447f28561723
Helen Lily Hu <hhusthere@gmail.com>
2021-01-20 23:32:04 -0600

    Set game board, pixel size dynamically

--- scripts/game.js
+++ scripts/game.js
@@ -1,9 +1,26 @@
 // initialize game board
+const PIXELSIZE = 20;
 const WIDTH = 10;
 const HEIGHT = 20;
+
+var gameBoard = document.getElementById('game-board');
+gameBoard.style.width = (WIDTH * PIXELSIZE).toString() + "px";
+gameBoard.style.height = (HEIGHT * PIXELSIZE).toString() + "px";
+
+// inserting rule into existing stylesheet doesn't work with Chrome
+// when running locally, possibly because of CORS
+// so create and use internal sheet
+var dynamicStyle = document.createElement("style");
+dynamicStyle.appendChild(
+  document.createTextNode(".pixel { \
+    width: " + PIXELSIZE.toString() + "px; \
+    height: " + PIXELSIZE.toString() + "px; }")
+);
+document.head.appendChild(dynamicStyle);
+
 for (var i = 0; i < WIDTH * HEIGHT; i++) {
   var pixel = document.createElement('div');
-  pixel.classList.add('test-div');
-  document.body.appendChild(pixel);
+  pixel.classList.add('pixel');
+  gameBoard.appendChild(pixel);
 }
 console.log('clear');
