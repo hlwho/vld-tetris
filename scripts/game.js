6daa26e930a55a5db9804f6f4ce71ddc2306c023
Helen Lily Hu <hhusthere@gmail.com>
2021-01-25 16:04:02 -0600

    Implement moving player block down
    - handle game board top overflow game over
    - handle top down block collision/stacking
    - exists bug where new block overwrites old block before game over b/c
    blocks don't enter the game board pixel by pixel right now

--- scripts/game.js
+++ scripts/game.js
@@ -3,8 +3,9 @@
 // dynamically loading and forcing the loaded script to execute is messy
 // and ES6 modules standard is overkill for this (intentionally vanilla) project
 const PIXELSIZE = 20;
-const GAME_WIDTH = 16; // 10
+const GAME_WIDTH = 10;
 const GAME_HEIGHT = 20;
+const PLAYER_START_POS = Math.floor(GAME_WIDTH / 2);
 
 // record block indices for each rotation of each block shape
 // imagining each block existing in a 4 x 4 square at the upper left-most
@@ -33,7 +34,7 @@ const iBlock = [
 const oBlock = [
   [0, 1, GAME_WIDTH, 1 + GAME_WIDTH]
 ];
-const blocks = [sBlock, tBlock, jBlock, iBlock, oBlock];
+const blockLst = [sBlock, tBlock, jBlock, iBlock, oBlock];
 var blockStyleClasses = ['s-block', 't-block', 'j-block', 'i-block', 'o-block']
 for (let i = 0; i < blockStyleClasses.length; i++) {
   blockStyleClasses[i] = ['block', blockStyleClasses[i]];
@@ -57,12 +58,16 @@ dynamicStyle.appendChild(
     width: ' + PIXELSIZE.toString() + 'px; \
     height: ' + PIXELSIZE.toString() + 'px; }')
 );
+// divs representing 'pixels' on the game board
 var pixelLst = [];
+// gameBoardMap[i] == 1 if there is a block filling that pixel; 0 otherwise
+var gameBoardMap = [];
 for (let i = 0; i < GAME_WIDTH * GAME_HEIGHT; i++) {
   let pixel = document.createElement('div');
   pixel.classList.add('pixel');
   gameBoard.appendChild(pixel);
   pixelLst.push(pixel);
+  gameBoardMap.push(0);
 }
 
 const startPauseButton = document.getElementById('start-pause-button');
@@ -76,33 +81,158 @@ dynamicStyle.appendChild(
     border-width: ' + BLOCK_BORDER_WIDTH.toString() + 'px; }')
 );
 
+
 /**
 * Draw block onto the gameBoard pixels (pixelLst) at location loc
 * and using the style classes in styleList
-* @param {number} loc the index of the location to start drawing at
-* @param {number array} block the pixel indices of the shape to draw
-* @param {string array} styleList the list of css classes to style blockRot with
+* @param {number} pos index of the position at which to start drawing
+* @param {number array} block pixel indices of the block to draw
+* @param {string array} styleList list of css classes with which to style block
 */
-function draw(loc, block, styleList) {
+function draw(pos, block, styleList) {
   block.forEach(idx => {
-    p = pixelLst[loc + idx];
+    let p = pixelLst[pos + idx];
     styleList.forEach(sc => {
       p.classList.add(sc);
     });
   });
-
 }
 
-function testBlocks(row, blockRots, styleList) {
-  blockRots.forEach((block, i) => {
-    draw(row + i * 4, block, styleList);
+/**
+* Erase block from the gameBoard pixels (pixelLst) at location loc
+* by unusing the style classes in styleList
+* @param {number} pos index of the position at which to start erasing
+* @param {number array} block pixel indices of the block to erase
+* @param {string array} styleList list of css classes with which block is styled
+*/
+function erase(pos, block, styleList) {
+  block.forEach(idx => {
+    let p = pixelLst[pos + idx];
+    styleList.forEach(sc => {
+      p.classList.remove(sc);
+    });
   });
-
 }
 
-blocks.forEach((blockRots, i) => {
-  testBlocks(i * 4 * GAME_WIDTH, blockRots, blockStyleClasses[i]);
-});
+
+// Configure player
+// idx of block currently under player control
+var playerBlockIdx;
+// idx of block's rotation
+var playerBlockRot;
+// position of player block on game board
+var playerBlockPos;
+
+/**
+* Initialize block under player's control
+*/
+function newPlayerBlock() {
+  playerBlockIdx = Math.floor(Math.random() * blockLst.length);
+  playerBlockRot = 0;
+  playerBlockPos = PLAYER_START_POS;
+}
 
 
-console.log('clear 41:02');
+// Set up game logic
+// interval ID which moves the player's block down
+var playerGravity;
+
+/**
+* Reset game board map such that all pixels are marked as unoccupied
+*/
+function clearGameBoardMap() {
+  for (let i = 0; i < gameBoardMap.length; i++) {
+    gameBoardMap[i] = 0;
+  }
+}
+
+/**
+* Reset game board such that no pixel divs appear to be blocks
+*/
+function clearGameBoard() {
+  // TODO: optimize runtime?
+  for (let i = 0; i < pixelLst.length; i++) {
+    let p = pixelLst[i];
+    blockStyleClasses.forEach(lst => {
+      lst.forEach(sc => {
+        p.classList.remove(sc);
+      });
+    });
+  }
+}
+
+/**
+* End the game
+*/
+function gameOver() {
+  clearInterval(playerGravity);
+  clearGameBoardMap();
+  console.log('GAME OVER');
+}
+
+/**
+* True if the block is at the bottom of the game board or on top of another
+* block; False otherwise
+* @param {number} pos index of the position of the block to check
+* @param {number array} block pixel indices of the block to check
+*/
+function blockLanded(pos, block) {
+  let hitBottomOrBlock = block.some(bidx => {
+    let nextPos = pos + bidx + GAME_WIDTH;
+    return nextPos >= GAME_WIDTH * GAME_HEIGHT || gameBoardMap[nextPos];
+  });
+  return hitBottomOrBlock;
+}
+
+/**
+* Mark the block as fixed on the game board map
+* @param {number} pos index of the position of the block to mark as fixed
+* @param {number array} block pixel indices of the block to mark as fixed
+*/
+function fixBlock(pos, block) {
+  block.forEach(idx => {
+    gameBoardMap[pos + idx] = 1;
+  });
+}
+
+/**
+* True if blocks have reached the top of the game board; False otherwise
+* @param {number} pos index of the position of the last block placed
+* @param {number array} block pixel indices of the last block placed
+*/
+function boardFilled(pos, block) {
+  return block.some(bidx => { return pos + bidx < GAME_WIDTH; });
+}
+
+/**
+* Game loop which handles moving the player-controlled block downwards
+*/
+function movePlayerDown() {
+  let block = blockLst[playerBlockIdx][playerBlockRot];
+  let switchBlock = blockLanded(playerBlockPos, block);
+  if (switchBlock && boardFilled(playerBlockPos, block)) {
+    gameOver();
+  } else if (switchBlock) {
+    fixBlock(playerBlockPos, block);
+    newPlayerBlock();
+    draw(playerBlockPos,
+      blockLst[playerBlockIdx][playerBlockRot],
+      blockStyleClasses[playerBlockIdx]);
+  } else {
+    let blockStyle = blockStyleClasses[playerBlockIdx];
+    erase(playerBlockPos, block, blockStyle);
+    playerBlockPos = playerBlockPos + GAME_WIDTH;
+    draw(playerBlockPos, block, blockStyle);
+  }
+}
+
+function startGame() {
+  clearGameBoard();
+  newPlayerBlock();
+  playerGravity = setInterval(movePlayerDown, 500);
+}
+
+startGame();
+
+
+console.log('clear 51:32');
